f26b7e56ccf2d7d6455b38a4ce839810b671c0d2
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-06 14:38:30 -0300

    Added two basic product routes on our controllers

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+const prodsRotas = require('./routes/products')
+const conexaoBD = require('./db/connect')
+
 
 const naoEncontradoMiddleware = require('./middleware/not-found');
 const  erroMiddleware = require('./middleware/error-handler')
@@ -14,6 +17,8 @@ app.use(express.json())
 app.get('/',(req,res) => {
     res.send('<h1>Store API</h1><a href="/api/v1/products">Rota dos Produtos</a>')
 })
+
+app.use('/api/v1/products',prodsRotas)
 
 
 //products routes 
@@ -29,6 +34,7 @@ const port = process.env.PORT || 3000
 const start = async () => {
     try {
         //Connect DB
+        await conexaoBD(process.env.MONGO_URI)
         app.listen(port,console.log(`Servidor está ouvindo na porta ${port}`))
     } catch (error) {
         console.log(error)
---
+++ controllers/products.js
@@ -0,0 +1,14 @@
+
+
+const pegaTodosProdutosEstatico = async (req,res) => {
+    res.status(200).json({msg:'produtos teste rota'})
+}
+
+const pegaTodosProdutos = async (req,res) => {
+    res.status(200).json({msg:'produtos teste rota'})
+}
+
+module.exports = {
+    pegaTodosProdutos,
+    pegaTodosProdutosEstatico,
+}
