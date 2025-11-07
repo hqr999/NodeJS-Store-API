a40b3b38858f31730bb45e10e596dc02447bf100
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-07 14:28:45 -0300

    Added the sort functionality into our API

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,13 @@ const { Query } = require('mongoose')
 const Product = require('../models/product')
 
 
-const getAllProductsStatic = async (req,res) => {
-    const products = await Product.find({featured:true})
-    res.status(200).json({products,number_elements:products.length})
+const getAllProductsStatic = async (req, res) => {
+    const products = await Product.find({}).sort('name')
+    res.status(200).json({ products, number_elements: products.length })
 }
 
-const getAllProducts = async (req,res) => {
-    const { featured } = req.query
-    const { company } = req.query 
+const getAllProducts = async (req, res) => {
+    const { featured,company,name, sort } = req.query
     const queryObject = {}
 
     if (featured) {
@@ -19,13 +18,23 @@ const getAllProducts = async (req,res) => {
     if (company) {
         queryObject.company = company
     }
-    
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+
     console.log(queryObject)
-    const products = await Product.find(queryObject)
-    res.status(200).json({products,number_elements:products.length})
+    let result  = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }else{
+        result = result.sort('createdAt')
+    }
+    const products = await result
+    res.status(200).json({ products, number_elements: products.length })
 }
 
 module.exports = {
-   getAllProducts,
-   getAllProductsStatic,
+    getAllProducts,
+    getAllProductsStatic,
 }
