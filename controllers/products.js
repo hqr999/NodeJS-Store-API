4d51cdb02fe6b8281d58cb2ce7cf4eea7726335f
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-07 13:10:51 -0300

    our getAllProducts handles featured and company

--- controllers/products.js
+++ controllers/products.js
@@ -1,18 +1,31 @@
+const { Query } = require('mongoose')
 const Product = require('../models/product')
 
 
-const pegaTodosProdutosEstatico = async (req,res) => {
+const getAllProductsStatic = async (req,res) => {
     const products = await Product.find({featured:true})
     res.status(200).json({products,number_elements:products.length})
 }
 
-const pegaTodosProdutos = async (req,res) => {
-    const products = await Product.find(req.query)
+const getAllProducts = async (req,res) => {
+    const { featured } = req.query
+    const { company } = req.query 
+    const queryObject = {}
 
+    if (featured) {
+        queryObject.featured = featured === 'true' ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company
+    }
+    
+    console.log(queryObject)
+    const products = await Product.find(queryObject)
     res.status(200).json({products,number_elements:products.length})
 }
 
 module.exports = {
-    pegaTodosProdutos,
-    pegaTodosProdutosEstatico,
+   getAllProducts,
+   getAllProductsStatic,
 }
