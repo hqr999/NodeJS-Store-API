433f9d3557374152c65ad8a2585b6eb09c930620
Henrique Queiroz <henrique.queiroz@protack.local>
2025-11-10 11:33:43 -0300

    Numeric filters added to the API

--- controllers/products.js
+++ controllers/products.js
@@ -1,15 +1,16 @@
-const { Query } = require('mongoose')
 const Product = require('../models/product')
 
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Product.find({}).sort('name').select('name price').limit(6)
+    const products = await Product.find({ price: { $gt: 34 } }).sort('price').select('name price')
     res.status(200).json({ products, number_elements: products.length })
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured,company,name, sort,fields } = req.query
+    const { featured, company, name, sort, fields, numericFilters } = req.query
     const queryObject = {}
+
+    
 
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false
@@ -18,16 +19,37 @@ const getAllProducts = async (req, res) => {
     if (company) {
         queryObject.company = company
     }
+
     if (name) {
-        queryObject.name = { $regex: name, $options: 'i'}
+        queryObject.name = { $regex: name, $options: 'i' }
+    }
+
+    if (numericFilters) {
+        const operatoMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+
+        }
+        const regEx = /\b(<|>|>=|=|<|<=)\b/g
+        let filters = numericFilters.replace(regEx, (match) => `-${operatoMap[match]}-`)
+        const options = ['price', 'rating']
+        filters = filters.split(',').forEach(element => {
+            const [field, operator, value] = element.split('-')
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        });
     }
 
     console.log(queryObject)
-    let result  = Product.find(queryObject)
+    let result = Product.find(queryObject)
     if (sort) {
         const sortList = sort.split(',').join(' ')
         result = result.sort(sortList)
-    }else{
+    } else {
         result = result.sort('createdAt')
     }
 
@@ -36,6 +58,13 @@ const getAllProducts = async (req, res) => {
         const fiedlsList = fields.split(',').join(' ')
         result = result.select(fiedlsList)
     }
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || 10
+    const skip = (page - 1) * limit
+
+    result = result.skip(skip).limit(limit)
+    //
+
     const products = await result
     res.status(200).json({ products, number_elements: products.length })
 }
